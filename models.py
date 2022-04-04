be6b3afdecd8edd22a2d3b199e63fd857974aece
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-04-04 16:13:01 +0200

    db structure / relations refactoring

--- models.py
+++ models.py
@@ -4,15 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Day(db.Model):
+class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
-    weather_data = db.relationship("WeatherData", backref="day", lazy=True)
+    weather_data = db.relationship("WeatherData", backref="log", lazy=True)
 
 
 class WeatherData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, nullable=False)
     temperature = db.Column(db.Float)
     humidity = db.Column(db.Float)
-    day_id = db.Column(db.Integer, db.ForeignKey("day.id"), nullable=False)
+    station_log_id = db.Column(db.Integer, db.ForeignKey("log.id"), nullable=False)
--- routes/api.py
+++ routes/api.py
@@ -1,27 +1,33 @@
 from flask import Blueprint, jsonify
-from models import Day
+from models import Log
 
 
 api_ = Blueprint("api", __name__)
 
 
 @api_.route("/api/days", methods=["GET"])
-def get_days():
-    return jsonify(days=[day.date.strftime("%m-%d-%Y") for day in Day.query.all()])
+def get_logged_days():
+    logged_days = []
+
+    for date in Log.query.all():
+        if date not in logged_days:
+            logged_days.append(date)
+
+    return jsonify(days=[date.date.strftime("%m-%d-%Y") for date in logged_days])
 
 
 @api_.route("/api/weather_data/<day_date>", methods=["GET"])
 def get_weather_data(day_date):
     data = []
 
-    matching_days = [day for day in Day.query.all() if day.date.strftime("%x") == day_date]
+    matching_days = [log for log in Log.query.all() if log.date.strftime("%m-%d-%Y") == day_date]
 
     if len(matching_days) > 0:
-        day = matching_days[0]
+        log_data = matching_days[0]
 
-        for data_entry in day.weather_data:
+        for data_entry in log_data.weather_data:
             data.append({
-                "time": data_entry.time.strftime("%H:%M:%S"),
+                "time": log_data.date.strftime("%H:%M:%S"),
                 "temperature": data_entry.temperature,
                 "humidity": data_entry.humidity,
             })
