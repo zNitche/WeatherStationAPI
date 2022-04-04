b5459a9316f1b62ae32a5b4a1a197eaec670b4d5
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-04-04 15:00:46 +0200

    added api endpoint for getting saved days dates

--- models.py
+++ models.py
@@ -4,5 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Sample(db.Model):
+class Day(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
--- routes/api.py
+++ routes/api.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify
+from models import Day
 
 
 api_ = Blueprint("api", __name__)
 
 
-@api_.route("/")
-def hello():
-    return jsonify(hello="Hello")
+@api_.route("/api/days")
+def get_days():
+    return jsonify(days=[day.date.strftime("%x") for day in Day.query.all()])
