e9bd01321a9f06eb1cc5629d2ba6722f66af3ff3
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-04-04 15:59:10 +0200

    added api endpoints for getting weather data for specified day

--- models.py
+++ models.py
@@ -7,3 +7,12 @@ db = SQLAlchemy()
 class Day(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
+    weather_data = db.relationship("WeatherData", backref="day", lazy=True)
+
+
+class WeatherData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime, nullable=False)
+    temperature = db.Column(db.Float)
+    humidity = db.Column(db.Float)
+    day_id = db.Column(db.Integer, db.ForeignKey("day.id"), nullable=False)
--- routes/api.py
+++ routes/api.py
@@ -5,6 +5,25 @@ from models import Day
 api_ = Blueprint("api", __name__)
 
 
-@api_.route("/api/days")
+@api_.route("/api/days", methods=["GET"])
 def get_days():
-    return jsonify(days=[day.date.strftime("%x") for day in Day.query.all()])
+    return jsonify(days=[day.date.strftime("%m-%d-%Y") for day in Day.query.all()])
+
+
+@api_.route("/api/weather_data/<day_date>", methods=["GET"])
+def get_weather_data(day_date):
+    data = []
+
+    matching_days = [day for day in Day.query.all() if day.date.strftime("%x") == day_date]
+
+    if len(matching_days) > 0:
+        day = matching_days[0]
+
+        for data_entry in day.weather_data:
+            data.append({
+                "time": data_entry.time.strftime("%H:%M:%S"),
+                "temperature": data_entry.temperature,
+                "humidity": data_entry.humidity,
+            })
+
+    return jsonify(data=data)
