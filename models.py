eb7e5d4d7ee9d4e29c51cba9289270e1ac3fb4a5
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2022-04-06 20:05:00 +0200

    database models refactoring

--- models.py
+++ models.py
@@ -4,14 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Log(db.Model):
+class WeatherLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False)
-    weather_data = db.relationship("WeatherData", backref="log", lazy=True)
-
-
-class WeatherData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
     temperature = db.Column(db.Float)
     humidity = db.Column(db.Float)
-    station_log_id = db.Column(db.Integer, db.ForeignKey("log.id"), nullable=False)
--- routes/api.py
+++ routes/api.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 import json
 from datetime import datetime
-from models import Log, WeatherData
+from models import WeatherLog
 from utils import db_utils
 from consts import ApiConsts, DataConsts, DateConsts
 
@@ -13,7 +13,7 @@ api_ = Blueprint("api", __name__)
 def get_logged_days():
     logged_days = []
 
-    for log in Log.query.all():
+    for log in WeatherLog.query.all():
         date_str = log.date.strftime(DateConsts.DAY_FORMATTING)
 
         if date_str not in logged_days:
@@ -26,21 +26,20 @@ def get_logged_days():
 def get_weather_data(day_date):
     data = []
 
-    matching_logs = [log for log in Log.query.all() if log.date.strftime(DateConsts.DAY_FORMATTING) == day_date]
+    matching_logs = [log for log in WeatherLog.query.all() if log.date.strftime(DateConsts.DAY_FORMATTING) == day_date]
 
     for matching_log in matching_logs:
-        for data_entry in matching_log.weather_data:
-            data.append({
-                DataConsts.TIME_KEY_NAME: matching_log.date.strftime(DateConsts.HOUR_FORMATTING),
-                DataConsts.TEMPERATURE_KEY_NAME: data_entry.temperature,
-                DataConsts.HUMIDITY_KEY_NAME: data_entry.humidity,
-            })
+        data.append({
+            DataConsts.TIME_KEY_NAME: matching_log.date.strftime(DateConsts.HOUR_FORMATTING),
+            DataConsts.TEMPERATURE_KEY_NAME: matching_log.temperature,
+            DataConsts.HUMIDITY_KEY_NAME: matching_log.humidity,
+        })
 
     return jsonify(data=data)
 
 
-@api_.route("/api/log", methods=["POST"])
-def add_station_log():
+@api_.route("/api/log/weather", methods=["POST"])
+def add_weather_log():
     response = ApiConsts.POST_FAILED_MESSAGE
 
     data = request.data
@@ -52,7 +51,7 @@ def add_station_log():
             temperature = parsed_data[DataConsts.TEMPERATURE_KEY_NAME]
             humidity = parsed_data[DataConsts.HUMIDITY_KEY_NAME]
 
-            data_log = Log(date=datetime.now(), weather_data=[WeatherData(temperature=temperature, humidity=humidity)])
+            data_log = WeatherLog(date=datetime.now(), temperature=temperature, humidity=humidity)
 
             db_utils.add_object_to_db(data_log)
 
