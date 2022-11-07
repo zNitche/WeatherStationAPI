eb40fe988d539f8c00b91303cacb4374e7bb9dcd
zNitche <53448802+zNitche@users.noreply.github.com>
2022-11-07 16:11:11 +0100

    added support for .env files

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
+import dotenv
 import os
+
+
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".env"))
 
 
 class AppConfig:
     CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-    DATABASE_PATH = os.environ.get("DATABASE_PATH", default=os.path.join(CURRENT_DIR, "data", "database.db"))
+    DATABASE_PATH = os.path.join(CURRENT_DIR, "data", "database.db")
 
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{DATABASE_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     APP_PORT = 8080
     APP_HOST = "0.0.0.0"
+
+    API_AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
